25dc3f116431488d607e314b2f0507050cb2f63a
Jon wick <jonw3821@outlook.com>
2023-01-24 10:42:28 -0800

    Initial commit

---
+++ app/src/main/java/com/example/sotabots_scouting_chargedup/Main.kt
@@ -0,0 +1,17 @@
+package com.example.sotabots_scouting_chargedup
+
+import android.app.Activity
+import android.os.Bundle
+import android.os.PersistableBundle
+
+class Main : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+        setContentView(R.layout.start)
+    }
+
+    override fun onResume() {
+        super.onResume()
+    }
+
+}
