a7972e3df7a6f86080077a951b94ca622bdf7ae6
Jon wick <jonw3821@outlook.com>
2023-01-25 10:51:26 -0800

    backend programmming maybe?

--- app/src/main/java/com/example/sotabots_scouting_chargedup/Main.kt
+++ app/src/main/java/com/example/sotabots_scouting_chargedup/Main.kt
@@ -3,8 +3,15 @@ package com.example.sotabots_scouting_chargedup
 import android.app.Activity
 import android.os.Bundle
 import android.os.PersistableBundle
+import android.view.View
+import android.widget.Button
+
 
 class Main : Activity() {
+    var Start = findViewById<Button>(R.layout.start.btstart)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
     override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
         super.onCreate(savedInstanceState, persistentState)
         setContentView(R.layout.start)
@@ -12,6 +19,18 @@ class Main : Activity() {
 
     override fun onResume() {
         super.onResume()
+    }
+    var data = mutableMapOf<String, Int>()
+
+    fun setData(view: View){
+        data.putIfAbsent(view.id.toString(), 0)
+        data.compute(view.id.toString()) { k , v ->
+            return@compute if(v != null) v + 1 else 1
+        }
+    }
+
+    fun configureButtonBindings(){
+        Start.setOnClickListener(){x -> setData(x)}
     }
 
 }
