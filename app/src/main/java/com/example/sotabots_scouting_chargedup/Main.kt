15bf11c8b02e38763ef05d1d2a33107f7ca90755
Jon wick <jonw3821@outlook.com>
2023-01-25 20:49:55 -0800

    Fixed a few bugs. added the basic backend functions. Added background image. imported initialization xml from prev year

--- app/src/main/java/com/example/sotabots_scouting_chargedup/Main.kt
+++ app/src/main/java/com/example/sotabots_scouting_chargedup/Main.kt
@@ -12,28 +12,49 @@ class Main : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.start)
+        configureButtonBindings()
     }
 
 
     override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
         super.onCreate(savedInstanceState, persistentState)
         setContentView(R.layout.start)
+        configureButtonBindings()
     }
+
 
     override fun onResume() {
         super.onResume()
     }
+
     var data = mutableMapOf<String, Int>()
 
+
+
+    fun initializeData(team: Int, match:Int){
+        data["Team"] = team
+        data["match"] = match
+    }
     fun setData(view: View){
+
+        var button: Button = findViewById(view.id)
         data.putIfAbsent(view.id.toString(), 0)
         data.compute(view.id.toString()) { k , v ->
             return@compute if(v != null) v + 1 else 1
         }
+        var text = button.hint.toString() + data[view.id.toString()].toString()
+        button.text = text
+
     }
 
     fun configureButtonBindings(){
-        Start.setOnClickListener(){x -> setData(x)}
+        val start = findViewById<Button>(R.id.start)
+        start.setOnClickListener(){x: View -> setPoseView()}
+
+    }
+
+    fun setPoseView(){
+        setContentView(R.layout.setpose)
     }
 
 }
